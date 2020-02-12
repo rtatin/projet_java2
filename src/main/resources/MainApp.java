97c3f46ac83eaae8b5f613f6480afa01b0027b35
Tatin <rtwam@hotmail.fr>
2020-02-12 15:37:39 +0100

    création xml

---
+++ src/main/resources/MainApp.java
@@ -0,0 +1,16 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
